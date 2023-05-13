c81a2af140cbc921ddfcd1c2f156d40941c01f98
Toru959 <80747436+Toru959@users.noreply.github.com>
2023-05-13 08:37:10 +0800

    viewの作成

---
+++ app/Http/Controllers/TweetController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TweetController extends Controller
+{
+    public function showTimelinePage(){
+        return view('layouts.timeline');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TweetController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/timeline', [TweetController::class, 'showTimelinePage']);
+
+require __DIR__.'/auth.php';
